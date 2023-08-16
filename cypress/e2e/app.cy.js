6d31cdb9203bca164fd4953d486096a14c16a372
Luiz Almada <luizalmada@yahoo.com>
2023-08-16 01:13:26 -0300

    Implementa Test Cypress

---
+++ cypress/e2e/app.cy.js
@@ -0,0 +1,16 @@
+describe("Cypress Class", () => {
+  it("Should filter correctly", () => {
+    cy.visit("http://localhost:3000/dummyList");
+
+    cy.get("li:contains(Metamask)");
+    cy.get("li:contains(Solidity)");
+    cy.get("li:contains(Ethereum)");
+
+    cy.get("input").type("metamask");
+
+    cy.get("li:contains(Metamask)").should("exist");
+    cy.get("li:contains(Solidity)").should("not.exist");
+    cy.get("li:contains(Ethereum)").should("not.exist");
+
+  })
+})
---
+++ cypress/integration/app.cy.js
@@ -0,0 +1,5 @@
+describe("Cypress Class", () => {
+  it("Should filter correctly", () => {
+    cy.visit("http://localhost:3000/dummyList")
+  })
+})
